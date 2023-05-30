b9b2af4b29c576ba0b52fa232960995bc5c99838
evermind <evermind@tuxfamily.org>
2023-05-30 14:48:35 +0200

    performance: create{Ext}DataPermUpdateScript() now runs much faster
    Performance in this functions was very bad due to:
    - context switches for sed and subshells calls
    
    Solution: replaced functionality with bash regexp.
    -> Performance up to 35 times faster. That is huge!

--- lib/functions_restore.sh
+++ lib/functions_restore.sh
@@ -7,44 +7,52 @@ function createDataPermUpdateScript() {
     local oldGidVar=$(($oldGid + 10000))
     local newGidVar=$(($newGid + 10000))
 
+    local re='OLDU=([0-9]+):OLDG=([0-9]+):FILENAME=(.*)'
+    local cmd="chown"
+
     echo "# dataDir: oldGid=$oldGid newGid=$newGid oldUid=$oldUid newUid=$newUid oldGidVar=$oldGidVar newGidVar=$newGidVar"
 
-    eval $AS "$BUSYBOX find $workDir ! -type l -exec $BUSYBOX stat -c \"chownPATTERNOLDU=%u:OLDG=%gPATTERNBEGINrogaPATTERN%nPATTERNENDrogaPATTERN\" {} +" \
+    eval $AS "$BUSYBOX find $workDir ! -type l -exec $BUSYBOX stat -c \"OLDU=%u:OLDG=%g:FILENAME=%n\" {} +" \
     | while read line ; do
-        line="`echo "$line" | sed -e 's@PATTERN@ @g' -e 's@\"@\\\\"@g' -e 's@BEGINroga @"@g' -e 's@ ENDroga @"@g'`"
-        edebug "oldOwner $line" | sed -e 's@OLDU=@@g' -e 's@OLDG=@@g' | sed 's@^@# @'
-        OLDU=`echo "$line" | egrep -o 'OLDU=[^:]*' | sed -e 's@OLDU=@@g'`
-        OLDG=`echo "$line" | egrep -o 'OLDG=[^ ]*' | sed -e 's@OLDG=@@g'`
+        [[ $line =~ $re ]]
+        OLDU=${BASH_REMATCH[1]}
+        OLDG=${BASH_REMATCH[2]}
+        FILE=${BASH_REMATCH[3]}
+        FILE="${FILE//\"/\\\"}"
+
+        edebug "# oldOwner $cmd $OLDU:$OLDG \"$FILE\""
         edebug "OLDU=$OLDU OLDG=$OLDG"
 
         # replace gid
+        local nGid="$OLDG"
         if [ "$OLDG" -ne "$oldGid" ] ; then
             # test if we have special GidVar
             if [ "$OLDG" -eq "$oldGidVar" ] ; then
                 # replace $oldGidVar with newGidVar
-                line="`echo "$line" | sed "s@OLDG=$oldGidVar@$newGidVar@g"`"
+                nGid="$newGidVar"
                 #echo "$OLDG" -eq "$oldGidVar"
             elif [ "$OLDG" -eq "0" ] ; then # keep do nothing eg lib dir is root only
-                line="`echo "$line" | sed "s@OLDG=0@0@g"`"
+                nGid="0"
             else
-                echo GIDERROR $line
+                echo "GIDERROR $cmd $OLDU:$OLDG \"$FILE\""
                 exit 1
             fi
         else
-            line="`echo "$line" | sed "s@OLDG=$oldGid@$newGid@g"`"
+            nGid=$newGid
         fi
 
         # replace uid
+        local nUid="$OLDU"
         if [ "$OLDU" -eq "$oldUid" ] ; then
-            line="`echo "$line" | sed "s@OLDU=$oldUid@$newUid@g"`"
+            nUid="$newUid"
         elif [ "$OLDG" -eq "0" ] ; then # keep do nothing eg lib dir is root only
-            line="`echo "$line" | sed "s@OLDU=0@0@g"`"
+            nUid="0"
         else
-            echo UIDERROR $line
+            echo "UIDERROR $cmd $OLDU:$OLDG \"$FILE\""
             exit 1
         fi
 
-        echo $line
+        echo "$cmd $nUid:$nGid \"$FILE\""
     done
 }
 
@@ -58,42 +66,51 @@ function createExtDataPermUpdateScript() {
     local newGidVar2=$(($newGid + 30000))
     local media_rw=1023
 
+    local re='OLDU=([0-9]+):OLDG=([0-9]+):FILENAME=(.*)'
+    local cmd="chown"
+
     echo "# extDataDir: oldGid=$oldGid newGid=$newGid oldGidVar=$oldGidVar newGidVar=$newGidVar"
 
-    eval $AS "$BUSYBOX find $workDir ! -type l -exec $BUSYBOX stat -c \"chownPATTERNOLDU=%u:OLDG=%gPATTERNBEGINrogaPATTERN%nPATTERNENDrogaPATTERN\" {} +" \
+    eval $AS "$BUSYBOX find $workDir ! -type l -exec $BUSYBOX stat -c \"OLDU=%u:OLDG=%g:FILENAME=%n\" {} +" \
     | while read line ; do
-        line="`echo "$line" | sed -e 's@PATTERN@ @g' -e 's@\"@\\\\"@g' -e 's@BEGINroga @"@g' -e 's@ ENDroga @"@g'`"
-        edebug "oldOwner $line" | sed -e 's@OLDU=@@g' -e 's@OLDG=@@g' | sed 's@^@# @'
-        OLDU=`echo "$line" | egrep -o 'OLDU=[^:]*' | sed -e 's@OLDU=@@g'`
-        OLDG=`echo "$line" | egrep -o 'OLDG=[^ ]*' | sed -e 's@OLDG=@@g'`
+        [[ $line =~ $re ]]
+        OLDU=${BASH_REMATCH[1]}
+        OLDG=${BASH_REMATCH[2]}
+        FILE=${BASH_REMATCH[3]}
+        FILE="${FILE//\"/\\\"}"
+        local preText=""
+
+        edebug "# oldOwner $cmd $OLDU:$OLDG \"$FILE\""
         edebug "OLDU=$OLDU OLDG=$OLDG"
 
         # replace gid
+        local nGid="$OLDG"
         if [ "$OLDG" -ne "$media_rw" ] ; then
             # test if we have special GidVar
             if [ "$OLDG" -eq "$oldGidVar" ] ; then
                 # replace $oldGidVar with newGidVar
-                line="`echo "$line" | sed "s@OLDG=$oldGidVar@$newGidVar@g"`"
+                nGid="$newGidVar"
             elif [ "$OLDG" -eq "$oldGidVar2" ] ; then
                 # replace $oldGidVar2 with newGidVar2
-                line="`echo "$line" | sed "s@OLDG=$oldGidVar2@$newGidVar2@g"`"
+                nGid="$newGidVar2"
             else
-                echo GIDERROR $line
+                echo "GIDERROR $cmd OLDU=$OLDU:OLDG=$OLDG \"$FILE\""
                 exit 1
             fi
         else
-            line="# skip as same perms `echo "$line" | sed "s@OLDG=$media_rw@$media_rw@g"`"
+            nGid=$media_rw
+            preText="# skip as same perms "
         fi
 
         # no need to replace uid as it should be $media_rw
         # -> just prepare $line executable
         if [ "$OLDU" -eq "$media_rw" ] ; then
-            line="`echo "$line" | sed "s@OLDU=$media_rw@$media_rw@g"`"
+            nUid="$media_rw"
         else
-            echo UIDERROR $line
+            echo "UIDERROR $cmd $OLDU:$OLDG \"$FILE\""
             exit 1
         fi
 
-        echo $line
+        echo "${preText}$cmd $nUid:$nGid \"$FILE\""
     done
 }
